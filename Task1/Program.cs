6d1fc147bdbe9170c6e19ec9fd872c8b828a2c28
Yusif70 <yusifpirquliyev7@gmail.com>
2024-06-01 11:40:42 +0400

    initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,104 @@
+﻿#region HackerRank
+
+#region cem
+
+//Console.Write("eded daxil edin: ");
+//int a = int.Parse(Console.ReadLine());
+//Console.Write("2ci ededi daxil edin: ");
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine();
+
+//static void solveMeFirst(int a,int b)
+//{
+//    int sum = a + b;
+//    Console.WriteLine("cem: " + sum);
+//}
+
+//solveMeFirst(a,b);
+
+#endregion
+
+#region arraySum
+
+//Console.Write("array olcusunu daxil edin: ");
+//int size = Convert.ToInt32(Console.ReadLine());
+
+//int[] ints = new int[size];
+
+//for (int i = 0; i < size; i++)
+//{
+//    ints[i] = Convert.ToInt32(Console.ReadLine());
+//}
+
+//void simpleArraySum(int[] ints)
+//{
+//    int sum = 0;
+//    for (int i = 0; i < size; i++)
+//    {
+//        sum += ints[i];
+//    }
+//    Console.WriteLine("cemi: " + sum);
+//}
+
+//simpleArraySum(ints);
+
+#endregion
+
+#endregion
+
+#region leetCode
+
+#region reverse integer
+
+//Console.Write("reqemlerin sayini daxil edin: ");
+//int reqemSayi = int.Parse(Console.ReadLine());
+
+//Console.Write("ededi daxil edin: ");
+//int x = int.Parse(Console.ReadLine());
+
+//int ters = 0;
+
+//for (int i = 1; i <= reqemSayi; i++)
+//{
+//	int a = (x % QuvvetiHesabla(10, i) - x % QuvvetiHesabla(10, i-1))/ QuvvetiHesabla(10, i-1);
+//	ters += a * QuvvetiHesabla(10, reqemSayi - i);
+//}
+
+//Console.WriteLine("ededin tersi: " + ters);
+
+//static int QuvvetiHesabla(int eded, int quvvet)
+//{
+//	int netice = 1;
+//	for (int i = 1; i <= quvvet; i++)
+//		netice *= eded;
+//	return netice;
+//}
+
+#endregion
+
+#region search position
+
+int[] nums = [1, 2, 3, 4, 5];
+
+Console.Write("axtaris ucun eded daxil edin: ");
+int target = int.Parse(Console.ReadLine());
+
+int indexOfTarget = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+	if (nums[i] == target)
+	{
+		indexOfTarget = i;
+		break;
+	}
+	else if(target > nums[i])
+	{
+		indexOfTarget++;
+	}
+}
+
+Console.WriteLine(indexOfTarget);
+
+#endregion
+
+#endregion
